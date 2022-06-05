188f256a877252511bcfa680de223b9de0ff3080
Timdit <timur0809tim@gmail.com>
2022-06-05 12:27:34 +0500

    vue

---
+++ app.js
@@ -0,0 +1,30 @@
+const App = {
+    data() {
+        return {
+            placeholderString: 'Введите название заметки',
+            title: 'Список заметок',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2'],
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
+        },
+        addNewNote() {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        removeNote(i) {
+            this.notes.splice(i, 1)
+        }
+    }
+}
+
+
+Vue.createApp(App).mount('#app')
